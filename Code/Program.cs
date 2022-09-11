6f5f7a7747c03001a27ff84f76d78f7f7f972410
Elena <malkovva@gmail.com>
2022-09-11 11:53:08 +0300

    Added solution to the task

---
+++ Code/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+string[] new_array = new string[4];
+int new_index = 0;
+for (int index = 0; index < 4; index++)
+{
+    if (cars[index].Length <= 3) new_array[new_index] = cars[index];
+    Console.WriteLine(new_array[new_index]);
+    new_index++;
+}
+
